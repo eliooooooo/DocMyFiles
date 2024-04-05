402951f53ebef19dc9969dd955b0722b69a58a60
eliooooooo <eliott.burkle@gmail.com>
2024-04-05 22:28:30 +0200

    Style: remove useless comments

--- tokenCounter.py
+++ tokenCounter.py
@@ -1,20 +1,4 @@
 import sys
-
-# METHOD 1
-# from tiktoken import Tokenizer
-# tokenizer = Tokenizer()
-
-# def countTokens(text):
-#     return len(list(tokenizer.tokenize(text)))
-
-# print(countTokens(sys.argv[1]))
-
-
-# METHOD 2
-# from openai_function_tokens import estimate_tokens
-# estimate_tokens(sys.argv[1], functions=None, function_call=None)
-
-# METHOD 3
 import tiktoken
 
 print(sys.argv[1])
