d98d422e343be3aba36d150ed5bbf2ca3935153d
eliooooooo <eliott.burkle@gmail.com>
2024-04-06 11:16:00 +0200

    Feat(counter): add token counter through tmp files

--- tokenCounter.py
+++ tokenCounter.py
@@ -1,7 +1,15 @@
 import sys
+import json
 import tiktoken
 
-print(sys.argv[1])
+# Open and read the tmp file
+filename = sys.argv[1]
+with open(filename, 'r') as f:
+    data = f.read()
+    f.close()
+
+# Parse the data as JSON
+data = json.loads(data)
 
 def num_token_from_string(string: str, encoding_name: str) -> int:
     """
@@ -17,4 +25,21 @@ def num_token_from_string(string: str, encoding_name: str) -> int:
     encoding = tiktoken.get_encoding(encoding_name)
     return len(encoding.encode(string))
 
-print(num_token_from_string(sys.argv[1], "cl100k_base"))
+def count_tokens(data):
+    """
+    Count the number of tokens in the content of each item in the data.
+
+    Args:
+        data: The data to count tokens in.
+
+    Returns:
+        The number of tokens in the data.
+    """
+    numTokens = 0
+    for item in data:
+        content = item['content']
+        numTokens += num_token_from_string(content, "cl100k_base")
+    return numTokens
+
+print(count_tokens(data))
+# print(num_token_from_string(data, "cl100k_base"))
